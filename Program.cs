0070259650c62d2828e64aa77b86e8d48d376bbe
ivolangen1 <iw.langen@student.avans.nl>
2024-11-28 12:54:57 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using SimpleMqtt;
+
+SimpleMqttClient client = SimpleMqttClient.CreateSimpleMqttClientForHiveMQ("laptop");
+await client.PublishMessage("uhm", "nadenken");
+
+await client.PublishMessage("Hoi hier test ben ik", "test");
+
+client.OnMessageReceived += (sender, args) => 
+{
+    Console.WriteLine($"Bericht ontvangen; topic={args.Topic}; message={args.Message}");
+};
+
+await client.SubscribeToTopic("TestMqtt");
+
+Console.ReadLine();
